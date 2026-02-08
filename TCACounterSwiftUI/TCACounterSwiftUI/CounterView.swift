be59c7fbde3b43e64ee15eae9829fb8f3854aa78
도미닉 <kiljh@me.com>
2026-02-08 18:47:44 +0900

    줄 맞춤

--- TCACounterSwiftUI/TCACounterSwiftUI/CounterView.swift
+++ TCACounterSwiftUI/TCACounterSwiftUI/CounterView.swift
@@ -9,34 +9,35 @@ import SwiftUI
 import ComposableArchitecture
 
 struct CounterView: View {
-  let store: StoreOf<CounterFeature>
-  
-  var body: some View {
-    VStack {
-      Text("\(store.count)")
-        .font(.largeTitle)
-        .padding()
-        .background(Color.black.opacity(0.1))
-        .cornerRadius(10)
-      HStack {
-        Button("-") {
-          store.send(.decrementButtonTapped)
+    let store: StoreOf<CounterFeature>
+    
+    var body: some View {
+        VStack {
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
+            
         }
-        .font(.largeTitle)
-        .padding()
-        .background(Color.black.opacity(0.1))
-        .cornerRadius(10)
-        
-        Button("+") {
-          store.send(.incrementButtonTapped)
-        }
-        .font(.largeTitle)
-        .padding()
-        .background(Color.black.opacity(0.1))
-        .cornerRadius(10)
-      }
     }
-  }
 }
 
 #Preview {
